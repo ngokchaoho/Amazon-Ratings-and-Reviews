cc5ca78c63263f100062259ecdc3703ce887f765
ngokchaoho <ngokchaoho@gmail.com>
2017-02-03 22:13:55 +0800

    Add files via upload
    Shell Script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+cd ~/Desktop/
+git clone git://github.com/ngokchaoho/Amazon-Ratings-and-Reviews.git
+apt-get install python-tk python-pip python-dev build-essential
+pip install bottlenose
+pip install lxml
+pip install python-dateutil
+pip install python-amazon-simple-product-api
+pip install amazon_scraper
+pip install wordcloud
+pip install numpy
+pip install pillow
+pip install matplotlib
+cd  ~/Desktop/Amazon-Ratings-and-Reviews/
+
